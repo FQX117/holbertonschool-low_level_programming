07f3a69bfe1763e0e2f34ba6fe0dd0e367b64787
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-24 14:05:01 -0600

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-char *variabletype[] = {"char", "unsigned char", "signed char", "int", "unsigned int", "short", "unsigned short", "long", "unsigned long", "long long", "unsigned long long"};
+char *variabletype[i] = {"char", "unsigned char", "signed char", "int", "unsigned int", "short", "unsigned short", "long", "unsigned long", "long long", "unsigned long long"};
 int main() 
 {printf("Size of %s is %u\n",variabletype[i], (unsigned int)(sizeof(variabletype[i])));
 return (0);
