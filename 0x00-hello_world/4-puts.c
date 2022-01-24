53e06beb117e0255ec7f94f9c22ade03cb834890
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-24 13:50:30 -0600

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
-/*not sure what i think of betty */ int main(void)
+/*
+*not sure what i think of betty */ int main(void)
 { puts("\"Programming is like building a multilingual puzzle");
 return (0); }
