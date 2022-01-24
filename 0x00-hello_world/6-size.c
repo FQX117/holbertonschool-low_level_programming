f7d6111ba99e8e163bf6ee2fd3514296301e08e3
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-24 14:21:25 -0600

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {char *variabletype[] = {"char", "float", "int", "short", "long", "long long"};
-{for (int i = 0; i < 11; ++i)
+for (int i = 0; i < 11; ++i)
 {printf("Size of %s is %u\n", variabletype[i], (unsigned int)(sizeof(variabletype[i])));
 }
 return (0);
