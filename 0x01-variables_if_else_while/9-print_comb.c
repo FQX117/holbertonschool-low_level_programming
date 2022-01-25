1877845d900036b32b6670e7f8abc3dec0f48de5
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-25 17:48:46 -0600

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 /*
 */int main(void)
+{
+int x;
+for (x = '0' ; x <= '9' ; x++)
+{
+putchar(x);
+if (x != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
