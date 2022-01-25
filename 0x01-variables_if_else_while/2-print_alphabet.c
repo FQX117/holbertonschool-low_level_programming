99d54cd13cf48abf09de0db35ae48148c849c0b5
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-25 17:25:00 -0600

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,10 +4,13 @@
 */int main(void)
 {
 int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+putchar(x);
+}
 for (x = 'A'; x <= 'Z'; x++)
 {
-int lower_x = tolower(x);
-putchar(lower_x);
+putchar(x);
 }
 putchar('\n');
 return (0);
