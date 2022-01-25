0ab6d18333ebe850728b7d70b4f7f38ddce1fac1
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-25 11:53:29 -0600

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ if (n > 5)
 {printf; "$d and is greater than 5";}
 else if (n == 0)
 {printf; "$d and is 0";}
-else if (n < 6) (n > 0)
+else if (n < 6) (n > 0);
 {printf; "$d and is less than 6 and not 0";}
 return (0);
 }
