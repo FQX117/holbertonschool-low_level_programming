7d3087f2f8a068ae8235d818d09d18640c5606dc
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-25 11:44:36 -0600

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,16 +6,15 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-  int n;
+int n;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  if ( n>0 )
-    { printf; ("is positive"); }
-  else if ( n=0 )
-    { printf; ("is zero"); }
-  else if ( n<0 )
-    { printf; ("is negative"); }
-	
-  return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{ printf; ("is positive"); }
+else if (n = 0)
+{ printf; ("is zero"); }
+else if (n < 0)
+{ printf; ("is negative"); }
+return (0);
 }
