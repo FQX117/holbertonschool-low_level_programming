dc9052aa5a61824c4bd3896230975da7d1c06b0d
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-25 17:46:41 -0600

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 /*
 */int main(void)
+{
+int a, b;
+int x;
+x = 'a';
+b = '0';
+for (a = 0 ; a < 16 ; a++)
+{
+if (a < 10)
+{
+putchar(b);
+b++;
+}
+else
+{
+putchar(x);
+x++;
+}
+}
+putchar('\n');
+return (0);
+}
