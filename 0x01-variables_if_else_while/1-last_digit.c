fc24ed0eef59014b801db1602c8850a0f96ff41b
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-25 16:04:09 -0600

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,9 +13,9 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 d = n % 10;
 if (n > 5)
-{ printf("Last digit of %d is %d and is greater than\n", n, d); }
+{ printf("Last digit of %d is %d and is greater than 5\n", n, d); }
 else if (n == 0)
-{ printf("Last digit of %d is %d and is 0\n", n, d); }
+{ printf("Last digit of %d is 0 and is 0\n", n); }
 else if (n < 6)
 { printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d); }
 return (0);
