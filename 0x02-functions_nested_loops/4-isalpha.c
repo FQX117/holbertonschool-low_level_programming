18cfbd7441acb69f6ca3cfdee3c4ef60a86c7500
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-26 23:24:08 -0600

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,19 @@
-
+#include "main.h"
+#include <stdio.h>
+/*
+*/
+int _isalpha(int x)
+{
+if (x >= 'a' && x <= 'z')
+{
+return (1);
+}
+else if (x >= 'A' && x <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
