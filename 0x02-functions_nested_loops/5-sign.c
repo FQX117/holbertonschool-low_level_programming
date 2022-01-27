2417c071b86d01a1e014eaf8d013b87aa629f566
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-26 23:27:42 -0600

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,22 @@
-
+#include "main.h"
+/*
+*/int print_sign(int x)
+{
+if (x > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (x == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+_putchar('\n');
+return (0);
+}
