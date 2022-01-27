c9687f7587fad06ea5de031fdd0b61566e3eb3ed
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-26 20:15:39 -0600

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* includes */
+#include <unistd.h>
+#include <stdio.h>
+/* prototypes */
+/* _putchar prototype */
 int _putchar(char c);
+/*print_alpha prot*/
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -7,7 +15,4 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#endif /* MAIN_H */
