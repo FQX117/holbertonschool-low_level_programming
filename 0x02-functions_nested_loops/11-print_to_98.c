9bdb1b99b84faa2a91a630f847c14f22499fa2d1
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-27 00:01:33 -0600

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,22 @@
-
+#include "main.h"
+#include <stdio.h>
+/*
+*/void print_to_98(int x)
+{
+if (x >= 98)
+{
+while (x > 98)
+{
+printf("%i, ", x--);
+}
+printf("%i\n", x);
+}
+else
+{
+while (x < 98)
+{
+printf("%i, ", x++);
+}
+printf("%i\n", x);
+}
+}
