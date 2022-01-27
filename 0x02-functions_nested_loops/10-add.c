4730d63ff1ab20684c677fd933e678d2c2cb518d
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-27 00:00:21 -0600

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include "main.h"
-/**
- * add - adds numbers
- * @x: first variable
- * @y: second variable
- * Return: (0)
- */
-int add(int x, int y)
+/*
+ */int add(int x, int y)
 {
 int result;
 {
