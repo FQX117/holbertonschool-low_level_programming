751d49d3b8bfd1dc46d558a1da52dca9f59572d9
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-26 23:37:06 -0600

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,20 @@
-
+#include "main.h"
+#include <stdio.h>
+/*
+*/
+int print_last_digit(int x)
+{
+n = (x % 10);
+if (x >= 0)
+{
+_putchar('0' + x);
+return (x);
+}
+else if (x < 0)
+{
+n = (x * -1);
+_putchar('0' + x);
+return (x);
+}
+return (0);
+}
