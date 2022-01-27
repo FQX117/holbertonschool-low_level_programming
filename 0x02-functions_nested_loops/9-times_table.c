b398603867e151f8d4fac47d1809204d4fdc749e
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-01-26 23:53:43 -0600

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,29 @@
-
+#include "main.h"
+#include <stdio.h>
+/*
+*/void times_table(void)
+{
+int l;
+int h;
+int p;
+for (l = 0; l <= 9; l++)
+{
+putchar('0');
+for (h = 1; h <= 9; h++)
+{
+p = l * h;
+p(',');
+p(' ');
+if (p <= 9)
+{
+p(' ');
+}
+else
+{
+p((p / 10) + '0');
+}
+p((p % 10) + '0');
+}
+p('\n');
+}
+}
