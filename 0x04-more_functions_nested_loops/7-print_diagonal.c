c38b56a43877179d00093fa1c3d41e8f4e1c41d3
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-01 21:06:13 -0600

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,23 @@
-
+#include "main.h"
+/*
+*/void print_diagonal(int n)
+{
+int x;
+int c;
+if (n > 0)
+{
+for (x = 0; x < n; x++)
+{
+for (c = 0; c < x; c++)
+{
+_putchar(' ');
+}
+_putchar('\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
