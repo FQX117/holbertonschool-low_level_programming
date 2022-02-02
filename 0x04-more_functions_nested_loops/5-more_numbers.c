dc3eba23448d16441da6daae131d77011ef56192
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-01 20:35:45 -0600

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,14 +1,19 @@
 #include "main.h"
-
 /*
-*/void print_numbers(void)
+ */void more_numbers(void)
 {
-char c;
-for (c = '0'; c <= '14'; c++)
+int x;
+int c;
+for (x = 1; x <= 10; x++)
 {
-_putchar(c);
+for (c = 0; c <= 14; c++)
+{
+if (c >= 10)
+{
+_putchar(c / 10 + '0');
 }
-{
+_putchar(c % 10 + '0');
+}
 _putchar('\n');
 }
 }
