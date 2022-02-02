2f1756539c236eac84fbf04813eca9ca7bef37bb
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-01 19:26:28 -0600

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,15 +3,15 @@
 /*
 */void print_most_numbers(void)
 {
-int c=0;
-while(c<=9)
+int c = 0;
+while (c <= 9)
 {
 if (c == 2 || c == 4)
 {
 c++;
 continue;
 }
-printf("%d" ,c);
+printf("%d", c);
 
 {
 
