8cccdd5a8fa2f6c85336165ec31728c1e0f0eefa
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-02 12:04:51 -0600

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "mani.h"
 //
-int main(void) 
+void reset_to_98(int *n); 
 {
-int num = 0;
-int *p = &num;
-*p=98;
-printf("%d\n", *p);
-return 0;
+    int num = 0;
+    int *n = &num;
+    char c = 98;
+    
+*n=98;
+
+
+putchar(c / 10 + '0');
+putchar(c % 10 + '0');
+    return 0;
 }
