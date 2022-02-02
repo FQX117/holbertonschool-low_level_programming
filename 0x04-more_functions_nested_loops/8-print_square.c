f708e6cc122cca9537ae29cb3ecf722a6711615b
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-01 21:20:16 -0600

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,22 @@
-
+#include "main.h"
+/*
+*/void print_square(int size)
+{
+int x;
+int c;
+if (size > 0)
+{
+for (x = 0; x < size; x++)
+{
+for (c = 0; c < size; c++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
