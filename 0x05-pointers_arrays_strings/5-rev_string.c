5efdac30e7738eb96d140c35a9bff9e4db6659c4
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-03 11:00:05 -0600

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,24 @@
-
+#include "main.h"
+/*
+*/void rev_string(char *s)
+{
+int c;
+int x;
+int tmp;
+int size;
+while (s[c] != 0)
+{
+c++;
+}
+size = c;
+i = 0;
+x = size - 1;
+while (c < x)
+{
+tmp = s[c];
+s[i] = s[x];
+s[x] = tmp;
+c++;
+x--;
+}
+}
