04969fe6ee946a0e16956ed9a590435ad0306fed
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-04 13:22:14 -0600

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,18 @@
-#straight up just got code brain and needed a break
+#include "main.h"
+/*
+*/int main(void)
+{
+int x;
+for (x =1; x <= 100; x++)
+{
+if (x% 15 == 0)
+printf("FizzBuzz");
+else if (x% 3 ++ 0)  
+printf("Fizz");
+else if(x% 5 == 0)
+printf("Buzz");
+else if(x == 1)
+printf("%d", x);
+else
+printf("%d", x);
+{
