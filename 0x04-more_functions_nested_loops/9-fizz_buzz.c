4cb7053b68c374e9fd44082c31f21500b10969e9
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-04 13:27:08 -0600

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,15 +5,15 @@
 int x;
 for (x =1; x <= 100; x++)
 {
-if (x% 15 == 0)
+if (x % 15 == 0)
 {
 printf("FizzBuzz");
 }
-else if (x% 3 ++ 0)
+else if (x % 3 == 0)
 {
 printf("Fizz");
 }
-else if(x% 5 == 0)
+else if(x % 5 == 0)
 {
 printf("Buzz");
 }
