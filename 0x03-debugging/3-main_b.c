3dba658ca0e38aa03a99a5d04945069e5ff0e6e3
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-04 09:53:32 -0600

    Update 3-main_b.c

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -1,27 +1,17 @@
 #include <stdio.h>
 #include "main.h"
-
-/**
- * main - takes a date and prints how many days are left in the year, taking
- * leap years into account
- * Return: 0
+/*
  */
-
 int main(void)
 {
-  int month;
-  int day;
-  int year;
-
-  month = 2;
-  day = 29;
-  year = 2000;
-
-  printf("Date: %02d/%02d/%04d\n", month, day, year);
-
-  day = convert_day(month, day);
-
-  print_remaining_days(month, day, year);
-
-  return (0);
+int m;
+int d;
+int y;
+m = 2;
+d = 29;
+y = 2000;
+printf("Date: %02d/%02d/%04d\n", m, d, y);
+day = convert_day(m, d);
+print_remaining_days(m, d, y);
+return (0);
 }
