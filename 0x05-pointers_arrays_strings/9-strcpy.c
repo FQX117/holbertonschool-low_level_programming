3ca7a637d8298a159b2b71cf30a58626118367e6
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-03 22:59:55 -0600

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,16 @@
-
+#include "main.h"
+/*
+*/char *_strcat(char *dest, char *src)
+{
+int c;
+int x;
+for (x = 0; dest[x] != '\0'; x++)
+;
+for (c = 0; src[c] != '\0'; c++)
+{
+dest[x] = src[c];
+i++;
+}
+dest[x] = '\0';
+return (dest);
+}
