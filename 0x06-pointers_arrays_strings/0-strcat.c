57c95632e78372c0d9da53ff0ea3683d7f7b16db
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 13:36:17 -0600

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,19 @@
-
+#include "holberton.h"
+/*
+*Write a function that concatenates two strings.
+*/char *_strcat(char *dest, char *src)
+{
+ int x, c;
+ x = 0;
+  while (*dest[x] != '\0')
+  {
+   ++x;
+  }
+  for  (c = 0; *src[c] != '\0'; ++c, ++x)
+  {
+    *dest[x] = src[c];
+  }
+  *dest[x] = '\0';
+  
+_putchar(*dest + 1);
+}  
