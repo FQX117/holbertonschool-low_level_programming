71fe1e08768b86e0d4cc682c2bf7ed40585ec8fc
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 15:37:32 -0600

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,10 @@
 *Write a function that copies a string.
 */char *_strncpy(char *dest, char *src, int n)
 {
- strcpy(src, dest);
-_strncpy (dest, src, n);
+int x, c;
+ for ( x =0; src[x] != '\0' && c < n; x++, c++)
+ {
+  dest[c] = src[x];
+ }
 return (dest);
 }
