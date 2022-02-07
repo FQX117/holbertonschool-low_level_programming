229747779e0b4069f477d37c1ac6bf13203a173d
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 14:32:13 -0600

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /*
 *Write a function that copies a string.
-*/char *_strncpy(char *dest, char *src, int n
+*/char *_strncpy(char *dest, char *src, int n)
 {
 _strncpy (dest, src, n);
 return (dest)
