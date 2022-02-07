580d38e6d2762b40b4b0e733ed174d9347bb3821
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 17:35:50 -0600

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,12 +2,16 @@
 /*
 */int _strcmp(char *s1, char *s2)
 {
-if (s1 == s2)
+if (s1 < s2)
 {
-return (0);
+return ('<0');
+}
+else if (s1 > s2)
+{
+return ('>0');
 }
 else
 {
-return(1);
+return(0);
 }
-}  
+}
