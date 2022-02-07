271cc8573fda374fe0a8f8a63c835816b75c6232
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 15:42:17 -0600

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,9 +4,9 @@
 */char *_strncpy(char *dest, char *src, int n)
 {
 int x, c;
- for ( x =0; src[x] != '\0' && c < n; x++, c++)
- {
-  dest[c] = src[x];
- }
+for (x = 0; src[x] != '\0' && c < n; x++, c++)
+{
+dest[c] = src[x];
+}
 return (dest);
 }
