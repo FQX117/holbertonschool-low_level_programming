6dfcd66947b4c3ff0e4e9a52d615ecff166d758a
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 19:36:40 -0600

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,12 @@
 */void reverse_array(int *a, int n)
 {
 int x;
-for (x = 0; x < n; x++)
-cout << a[x] << " ";
-cout << endl;
+while (a < n)
+{
+x = arr[a];
+arr[a] = arr[n];
+arr[n] = x;
+a++;
+n--;
+}
 }
