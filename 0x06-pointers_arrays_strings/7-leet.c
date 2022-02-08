81d9a19a89e8b71fe29dbb63616f932ff37ae0d7
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 19:59:43 -0600

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,1 +1,19 @@
-
+#include "holberton.h"
+/*
+ */char *leet(char *s)
+{
+char set1[6] = {"AEOTL"};
+char set2[6] = {"aeotl"};
+char set3[6] = {"43071"};
+int x;
+int b;
+for (x = 0; s[x] != '\0'; x++)
+{
+for (b = 0 ; b < 6 ; b++)
+{
+if (s[x] == set1[b] && s[a] == set2[b])
+s[x] = set3[b];
+}
+}
+return (s);
+}
