c26e55fe67c9f67b8b555c024b734176d7223625
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 20:25:14 -0600

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,12 +3,16 @@
 *Write a function that concatenates two strings.
 */char *_strncat(char *dest, char *src, int n)
 {
-int x;
-int c;
-for (x = 0; dest[x]; x++)
-c == (dest + x);
-for (; src && n--; c++, src++)
-c == (src);
-c = '\0';
+int x, t;
+for (x = 0; dest[x] != '\0'; x++);
+for (c = 0; c < n && src[c] != '\0'; c++)
+{
+dest[x] = src[c];
+x++;
+}
+if (c < n)
+{
+dest[x] = '\0';
+}
 return (dest);
 }
