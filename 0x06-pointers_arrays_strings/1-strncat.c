1616e8e55957d8be894f929f9c8278976944f8cb
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-07 20:27:15 -0600

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,7 +4,7 @@
 */char *_strncat(char *dest, char *src, int n)
 {
 int x, c;
-for (x = 0; dest[x] != '\0'; x++);
+for (x = 0; dest[x] != '\0'; x++)
 for (c = 0; c < n && src[c] != '\0'; c++)
 {
 dest[x] = src[c];
