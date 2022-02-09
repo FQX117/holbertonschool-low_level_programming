07da3088608db37a6b379a2eb6d9fb2b6dbbcd9c
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-09 17:26:51 -0600

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,13 @@
 #include "main.h"
-
-void _puts_recursion(char *s)
+/*
+*/void _puts_recursion(char *s)
 {
   if (*s != '\0')
-  {
+    {
     _putchar (*s);
     s++;
-_puts_recursion (s);
-  
-    
-  }
-  else
-  _putchar ('\n');
+    _puts_recursion (s);
+    }
+else
+_putchar ('\n');
 }
