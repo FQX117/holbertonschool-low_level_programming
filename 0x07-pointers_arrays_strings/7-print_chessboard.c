97682d9b4c1da9527d9b0dc43b86b1688b106fef
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-08 20:05:33 -0600

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,17 +2,13 @@
 /* 
 */void print_chessboard(char (*a)[8])
 {
-int x;
-int c;
-for (x = 0; x < 8; x++)
-{
+int c, x;
 for (c = 0; c < 8; c++)
 {
-_putchar(c);
-if (a[c] == a[8])
+for (x = 0; x < 8; x++)
 {
+_putchar(a[c][x]);
+}
 _putchar('\n');
-}
-}
 }
 }
