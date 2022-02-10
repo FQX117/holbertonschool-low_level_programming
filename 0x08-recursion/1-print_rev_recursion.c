aac8d2d9a2518005218a7c5c3f3724e78a98432d
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-09 19:36:40 -0600

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,11 @@
 # include "main.h"
-#include <string.h>
-void _print_rev_recursion(char *s)
+/*
+*/void _print_rev_recursion(char *s)
 {
-  
-if (*s != '\0')
+if (*s == '\0')
 {
-_print_rev_recursion(++s);
-_putchar (*s,\n);
+return;
 }
-  
- return;
-  
-
+_print_rev_recursion(++s);
+_putchar (*s);
 }
