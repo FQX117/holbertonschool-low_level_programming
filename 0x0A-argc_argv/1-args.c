87c9a755ff8f9bfa0684b13b752e6f98d653ac15
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-15 13:55:08 -0600

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /*
-*/ int main(int argc, char *argv[]__attribute__((unused)))
+*/int main(int argc, char *argv[])
 {
 int x = 0;
 for (; x < argc; x++)
-
-printf("argv[x]");
+printf("argv[%2d] : %s\n", x ,argv[x]);
 return (0);
 }
