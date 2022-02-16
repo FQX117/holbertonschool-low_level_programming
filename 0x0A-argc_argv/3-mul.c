ae97ad0525b9ca72c45d0b24e97f1f4aa37728c4
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-15 21:51:46 -0600

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 /*
 */ int main(int argc, char *argv[])
+
 {
 if (argc > 1 && argc < 4)
 {
-    int total = 1;
-    int one = 1;
-    for (; total < argc; total++)
-    {
-        one *= atoi(argv[total]);
-    }
-printf ("%d\n", one);
+int total = 1;
+int one = 1;
+for (; total < argc; total++)
+{
+one *= atoi(argv[total]);
+}
+printf("%d\n", one);
 }
 else
-printf ("error\n");
+printf("error\n");
 return(0);
 }
