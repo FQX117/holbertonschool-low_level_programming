e430d5b4e58514677c9dd8925a3fcea01946784e
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-15 22:25:41 -0600

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,23 +3,18 @@
 /*
 */ int main(int argc, char *argv[])
 {
+int total = 1;
+int one = 1
+{
 if (argc != 3)
 {
-   int total = 1;
-    int one = 1
-    {
-    printf("Error\n");
-    return (1);
+printf("Error\n");
+return (1);
 }   
-
-
-
-   
-    for (; total < argc; total++)
-    {
-        one *= atoi(argv[total]);
-    }
+for (; total < argc; total++)
+{
+one *= atoi(argv[total]);
+}
 printf ("%d\n", one);
-   
 return(0);
 }
