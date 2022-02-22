7eb2463f5e7fbcd5a2979fbe65e9c081a083e004
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-21 18:23:40 -0600

    Add files via upload

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,7 +3,12 @@
 */char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
-    int x, c;
 _strcat(*s1, *s2);
-return(s1);
+n = *s1;
+*n = malloc (sizeof(*n));
+if (n != NULL)
+{
+return (NULL);
+}
+return(0);
 }
