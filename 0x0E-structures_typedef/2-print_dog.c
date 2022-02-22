e689e7f3be989e9f5ef7110272dc5bd1d90bf22c
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-22 15:35:24 -0600

    Add files via upload

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,18 +14,18 @@
         printf("(nil)\n");
     
     else
-    printf("%s\n", d->name);
+    printf("Name: %s\n", d->name);
     
     if (d->age == 0 )
     
         printf("(nil)\n");
     
     else
-    printf("%f\n", d->age);
+    printf("Age: %f\n", d->age);
     if (d->owner == NULL)
     
         printf("(nil)\n");
     
     else
-    printf("%s\n", d->owner);
+    printf("Owner: %s\n", d->owner);
 }
