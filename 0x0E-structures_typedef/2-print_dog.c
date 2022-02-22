2bea47597f959a9f30e28778b6f2d2c1ae2ccc7f
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-22 15:23:16 -0600

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -4,29 +4,28 @@
 
 {
     if (d == NULL)
-    {
+    
         return;
-    }
+    
     else
-    {
+    
     if (d->name == NULL)
-    {
+    
         printf("%s\n", "(nil)");
-    }
+    
     else
     printf("%s\n", d->name);
     
     if (d->age == 0 )
-    {
+    
         printf("%s\n", "(nil)");
-    }
+    
     else
     printf("%f1\n", d->age);
     if (d->owner == NULL)
-    {
+    
         printf("%s\n", "(nil)");
-    }
+    
     else
     printf("%s\n", d->owner);
-}
 }
