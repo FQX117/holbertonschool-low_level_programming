94dc199def65242bff310cfa7c7ef95e01c86951
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-22 13:25:21 -0600

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,9 +2,9 @@
 /*
 */void init_dog(struct dog *d, char *name, float age, char *owner)
 {
- struct dog d = {
- .name = name,
- .age = age,
- .owner = owner
+{
+ d->name = name;
+ d->age = age;
+ d->owner = owner;
 };
 }
