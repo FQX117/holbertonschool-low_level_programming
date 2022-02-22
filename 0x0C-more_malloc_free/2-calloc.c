9b0e764065d70943af8e9aacbd8c699c21f5cf65
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-21 20:21:12 -0600

    Add files via upload

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+*/void *_calloc(unsigned int nmemb, unsigned int size)
+
+{
+	unsigned int x, y;
+	char *new;
+if (nmemb == 0 || size == 0)
+return (NULL);
+y = (nmemb * size);
+new = malloc(y);
+if (new == NULL)
+return (NULL);
+for (x = 0; x < y; x++)
+{
+new[x] = 0;
+}
+return (new);
+}
