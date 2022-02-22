7ae53c184b2c7880f337bcd0253accf69bab84fc
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-21 20:13:01 -0600

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,13 +3,14 @@
 */char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
+char new[50] = "";
+_strcpy(new, s1);   
+_strcat(new, s2);
 void *x = malloc(sizeof(n));
-char *_strcat(char *s1, char *s2);
-x = s1;
-
 if (x != NULL)
 {
 return (NULL);
 }
-return(x);
+
+return(new);
 }
