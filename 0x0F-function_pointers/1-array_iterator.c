14eb0d3dd132dc345f62c2625c3fa29d3491e29e
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-23 19:00:29 -0600

    Add files via upload

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
-/*this executes a fun of a fun
+/*
+*this executes a fun of a fun
 */void array_iterator(int *array, size_t size, void (*action)(int))
 
 {
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,7 @@
 #include "function_pointers.h"
-/*this dose a thing and i know hwo to find the thing not really sure why best guess just tells me if its positive could be usful
+/*
+*this dose a thing and i know hwo to find the thing not really 
+*sure why best guess just tells me if its positive could be usful
 */int int_index(int *array, int size, int (*cmp)(int))
 
 {
@@ -12,5 +14,5 @@ for (; x < size; x++)
 if (cmp(array[x]))
 return (x);
 }
-return (0);
+return (-1);
 }
