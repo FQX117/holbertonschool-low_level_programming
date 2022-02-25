61ff4aa2d950c318ad80820c807223efefb45967
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-25 15:29:52 -0600

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,8 +3,8 @@
 *im trying somthing new check the header it dint work
 */int sum_them_all(const unsigned int n, ...)
 {
-unsigned int x, total = 0;
 va_list ap;
+unsigned int x, total = 0;
 if (n == 0)
 return (0);
 va_start(ap, n);
