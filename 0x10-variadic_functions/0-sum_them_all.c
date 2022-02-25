159afb9f1d25a3356ed8b62de888809fd2863031
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-25 15:15:07 -0600

    Add files via upload

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,15 +2,14 @@
 /*
 *im trying somthing new check the header it dint work
 */int sum_them_all(const unsigned int n, ...)
-
 {
-unsigned int x, total = 0;
-va_list ap;
-if (n == 0)
-return (0);
-va_start(ap, n);
-for (x = 0 ; x < n; x++)
-total += va_arg(ap, int);
-va_end(ap);
-return (total);
+    unsigned int x, total = 0;
+    va_list ap;
+    if (n == 0)
+    return (0);
+    va_start(ap, n);
+    for (x = 0 ; x < n; x++)
+    total += va_arg(ap, int);
+    va_end(ap);
+    return (total);
 }
