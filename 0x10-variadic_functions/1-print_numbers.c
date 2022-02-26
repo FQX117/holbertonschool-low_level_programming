7adcc98f6bf4e46e98fac1468ed642f3f6aef87d
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-25 22:09:13 -0600

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 /*
 * so now we will just print nember just random nuembers jk there not random
 */void print_numbers(const char *separator, const unsigned int n, ...)
