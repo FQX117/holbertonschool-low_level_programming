e7713a1dd5f3265e601a8acc036811947caba954
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-02-25 22:14:02 -0600

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,13 +9,13 @@ unsigned int x;
 va_list ap;
 if (!separator)
 {
-separator = 0;
+return;
 }
 va_start(ap, n);
 for (x = 0 ; x < n; x++)
 {
 printf("%d", va_arg(ap, int));
-if (x < n - 1)
+if (x < n - 1 && separator != 0)
 printf("%s", separator);
 }
 printf("\n");
