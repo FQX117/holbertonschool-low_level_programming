d4ed85e5a54685830583a46d5875e99624b88a4f
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-07 22:20:57 -0600

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,6 +8,7 @@ for (; c[x]; x++)
 return (x);
 }
 
+#include "lists.h"
 /*
 */list_t *add_node(list_t **head, const char *str)
 {
@@ -22,8 +23,8 @@ if (str != NULL)
     c = strdup(str);
     x = _strlen(str);
 }
-c = strdup(str);
-x = _strlen(str);
+new->len = x;
+new->str = c;
 add->next = *head;
 *head = add;
 return (*head);
