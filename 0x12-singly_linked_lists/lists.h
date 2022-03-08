640907be44abc08efdeb9906931a54a65777e9fb
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-07 21:35:50 -0600

    Add files via upload

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
+#ifndef LISTS_H_
+#define LISTS_H_
 /** Headers **/
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,10 +13,12 @@
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(const char *s);
 #endif
