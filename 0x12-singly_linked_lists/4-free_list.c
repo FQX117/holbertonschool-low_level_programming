e9f6fccdd661bb753d8fc26bf5153233c891a385
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-07 22:33:53 -0600

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,14 +2,12 @@
 /*
 */void free_list(list_t *head)
 {
-	list_t *x;
-
-	while (head != NULL)
-	{
-		x = head->next;
-		free(head->str);
-		free(head);
-		head = x;
-	}
+list_t *x;
+while (head != NULL)
+{
+x = head->next;
+free(head->str);
+free(head);
+head = x;
 }
-  
+}
