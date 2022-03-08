53961f5194e43bac7966c02f7af59a063842db6e
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-07 22:45:02 -0600

    Update 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,31 +2,30 @@
 /*
 */list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *c, *p;
-	unsigned int len = 0;
-
-	c = malloc(sizeof(list_t));
-	if (c)
-	{
-		for (; *(str + len); len++)
-			c->str = strdup(str);
-		if (c->str)
-		{
-			c->len = len;
-			c->next = NULL;
-			if (!*head)
-			{
-				*head = c;
-				return (c);
-			}
-			p = *head;
-			while (p->next)
-				p = p->next;
-			p->next = c;
-			return (c);
-		}
-		free(c);
-		return (NULL);
-	}
-	return (NULL);
+list_t *c, *p;
+unsigned int len = 0;
+c = malloc(sizeof(list_t));
+if (c)
+{
+for (; *(str + len); len++)
+c->str = strdup(str);
+if (c->str)
+{
+c->len = len;
+c->next = NULL;
+if (!*head)
+{
+*head = c;
+return (c);
+}
+p = *head;
+while (p->next)
+p = p->next;
+p->next = c;
+return (c);
+}
+free (c);
+return (NULL);
+}
+return (NULL);
 }
