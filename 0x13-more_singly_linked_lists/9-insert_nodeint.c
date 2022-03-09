e8abb0cefc48ccaa9bd6e77c59fa10c91e320dc6
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-09 13:17:05 -0600

    Add files via upload

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,29 +2,37 @@
 /*
 */listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *cnt, *nn;
-unsigned int x = 0;
-/** nn = new node **/
+listint_t *nn, *p;
+unsigned int count = 1;
+if (head)
+{
 nn = malloc(sizeof(listint_t));
-if (!nn || !nn)
+if (nn == NULL)
 return (NULL);
 nn->n = n;
-if (idx == 0)
+if (idx > 0)
+{
+p = *head;
+while (p)
+{
+if (count == idx)
+{
+nn->next = p->next;
+p->next = nn;
+return (nn);
+}
+p = p->next;
+count++;
+}
+if (idx > count)
+return (NULL);
+}
+else
 {
 nn->next = *head;
 *head = nn;
+}
 return (nn);
 }
-if (*head = 0)
 return (NULL);
-cnt = *head;
-for (; x < idx -1; x++)
-{
-if (!cnt)
-return (NULL);
-cnt = cnt->next;
-}
-nn->next = cnt->next;
-cnt->next = nn;
-return (nn);
 }
