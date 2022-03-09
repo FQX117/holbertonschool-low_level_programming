25c170b9a7d81084940198324717d1908b3661f5
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-09 12:07:11 -0600

    Add files via upload

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,11 +3,11 @@
 */size_t print_listint(const listint_t *h)
 {
 int x = 0;
-const listint_t *x = h;
-for (; h; x++)
+const listint_t *c = h;
+for (; c; x++)
 {
-printf ("%d\n", h->n);
-h = h->next;
+printf ("%d\n", (*c).n);
+c = (*c).next;
 }
 return (x);
 }
