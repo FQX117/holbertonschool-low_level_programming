6830fccee4c866f5bb85415c54dd9a90a748a83e
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-09 13:19:17 -0600

    Add files via upload

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,32 +1,28 @@
 #include "lists.h"
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	unsigned int count;
-	listint_t *x, *p;
-
-	if (head && *head)
-	{
-		if (!index)
-		{
-			x = *head;
-			*head = (*head)->next;
-			free(x);
-			return (1);
-		}
-		p = *head;
-		for (count = 1; p; count++)
-		{
-			if (count == (index))
-			{
-				x = p;
-				p = p->next;
-				x->next = p->next;
-				free(p);
-				return (1);
-			}
-			p = p->next;
-		}
-		return (-1);
-	}
-	return (-1);
+unsigned int count = 1;
+listint_t *new = *head, *temp;
+if (!head || !*head)
+return (-1);
+if (index == 0)
+{
+*head = new->next;
+free(new);
+return (1);
+}
+temp = *head;
+while (temp)
+{
+if (count == index)
+{
+new = temp->next;
+temp->next = new->next;
+free(new);
+return (1);
+}
+temp = temp->next;
+count++;
+}
+return (-1);
 }
