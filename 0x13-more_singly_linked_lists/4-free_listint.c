2bd702ae1f5288bf0f9a0de23cc8db91024161ea
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-09 12:46:27 -0600

    Add files via upload

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,13 +2,13 @@
 /*
 */void free_listint(listint_t *head)
 {
-	listint_t *p;
-	if (!head)
-		return;
-		while (head)
-		{
-			p = head;
-	head = head->next;
-	free(p);
-		}
+listint_t *p;
+if (!head)
+return;
+else if (head)
+{
+p = head;
+head = head->next;
+free(p);
+}
 }
