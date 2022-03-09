6018ddc566ca95aa3e21225d1a1a43d5a5a27e78
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-09 12:30:07 -0600

    Add files via upload

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,23 +2,24 @@
 /*
 */listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *nn, *cn;
-/** nn = new node cn = curent node **/
-	nn = malloc(sizeof(*nn));
-	if (nn)
+	listint_t *x;
+	listint_t *p;
+
+	if (!head)
+		return (NULL);
+	p = *head;
+	x = malloc(sizeof(listint_t));
+	if (!x)
+		return(NULL);
+	x->n = n;
+	x->next = NULL;
+	if (*head == NULL)
 	{
-	nn->n = n;
-	nn->next = NULL;
-	if (*head)
-	{
-		cn = *head;
-		while (cn->next)
-		cn = cn->next;
-		cn->next = nn;
+		*head = x;
 		return (*head);
 	}
-	cn = *head;
+	while (p->next)
+		p = p->next;
+	p->next = x;
 	return (*head);
-}
-return (NULL);
 }
