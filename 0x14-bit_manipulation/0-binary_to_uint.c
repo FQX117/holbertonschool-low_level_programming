4288660389a0e528b311c312c1b9081706822b52
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-11 15:36:50 -0600

    Add files via upload

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,27 +2,24 @@
 /*
 */unsigned int binary_to_uint(const char *b)
 {
-unsigned int x;
-unsigned int c;
+unsigned int x = 0, count = 0, total = 0;
 if (b == NULL)
 return (0);
-for (c = 0; c != '\0'; c++)
+x = _strl(b);
+while (x--)
 {
-if ((b[c] != 'b') && (b[c] != '0') && (b[c] != '1'))
+if (b[x] != 48 && b[x] != 49)
 return (0);
-else
+if (b[x] == 49)
+total += 1 << count;
+count++;
+}
+return (total);
+}
+int _strl(const char *s)
 {
-while (c != '\0')
-{
-if (b[c] == '1')
-x = c * 2;
-else if (b[c] == '\0' - 1)
-{
-if (b[c] == '1')
-x = x + 1;
-}
-}
-}
-}
-return (x);
+int i = 0;
+for (; s[i] != '\0'; i++)
+;
+return i;
 }
