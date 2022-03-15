4ec303f02b740b9bbfe24007ab67725e13227fe9
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-15 18:32:44 -0500

    Add files via upload

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,12 +3,14 @@
 */size_t read_textfile(const char *filename, size_t letters)
 {
 int x;
+char *c;
 x = open(filename, O_RDWR);
+c = malloc(sizeof(char) * letters);
 if (!filename)
 return (0);
 else if (x == -1)
 return (0);
-read(x, letters);
-write(x, letters);
+read(x, c, letters);
+write(x, c, letters);
 return (letters);
 }
