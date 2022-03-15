de2a21e54b083719eefe00db8223a122a3648094
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-15 18:16:40 -0500

    Add files via upload

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,5 +2,13 @@
 /*
 */size_t read_textfile(const char *filename, size_t letters)
 {
-
+int x;
+x = open(filename, O_RDWR);
+if (!filename)
+return (0);
+else if (x == -1)
+return (0);
+read(x, letters);
+write(x, letters);
+return (letters);
 }
