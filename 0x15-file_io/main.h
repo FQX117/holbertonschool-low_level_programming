93ea55f8b587de74272057e04823cc77fc0dc490
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-03-16 10:36:53 -0500

    Add files via upload

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,14 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
+
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-void _run(int);
-int _putchar(char c);
-size_t read_textfile(const char *filename, size_t letters);
+
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 #endif
