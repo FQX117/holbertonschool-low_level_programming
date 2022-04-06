672c8342947d3ed1019aaca31c1dbf0f5d4c1809
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-04-06 15:47:26 -0500

    Add files via upload

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -12,18 +12,13 @@ char *key;
 char *value;
 struct hash_node_s *next;
 } hash_node_t;
-/**
-* struct hash_table_s - Hash table data structure
-*
-* @size: The size of the array
-* @array: An array of size @size
-* Each cell of this array is a pointer to the first node of a linked list,
-* because we want our HashTable to use a Chaining collision handling
+/*
 */typedef struct hash_table_s
 {
 unsigned long int size;
 hash_node_t **array;
 } hash_table_t;
+
 /**
 * prototypes
 */hash_table_t *hash_table_create(unsigned long int size);
