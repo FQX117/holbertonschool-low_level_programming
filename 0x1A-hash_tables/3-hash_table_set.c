1873f0e996d35c042e963164a8600f678857336f
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-04-06 15:52:30 -0500

    Add files via upload

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,37 +2,50 @@
 /*
 */int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int index;
-hash_node_t *currNode = malloc(sizeof(hash_node_t));
-hash_node_t *tempNode;
-if (!key || !ht || !currNode || strcmp("", key) == 0)
-{
-free(currNode);
+hash_node_t *element;
+if (ht == NULL)
 return (0);
+element = malloc(sizeof(hash_node_t));
+if (element == NULL)
+return (0);
+element->key = strdup(key);
+element->value = strdup(value);
+insert_element_list(ht, element);
+return (1);
 }
-index = key_index((const unsigned char *)key, ht->size);
-tempNode = ht->array[index];
-currNode->key = strdup(key);
-currNode->value = strdup(value);
-currNode->next = NULL;
-if (ht->array[index] == NULL)
-ht->array[index] = currNode;
+/*
+ */void insert_element_list(hash_table_t *ht, hash_node_t *element)
+{
+unsigned long int index;
+hash_node_t *tmp;
+index = key_index((unsigned char *)element->key, ht->size);
+tmp = ht->array[index];
+if (ht->array[index] != NULL)
+{
+tmp = ht->array[index];
+while (tmp != NULL)
+{
+if (strcmp(tmp->key, element->key) == 0)
+break;
+tmp = tmp->next;
+}
+if (tmp == NULL)
+{
+element->next = ht->array[index];
+ht->array[index] = element;
+}
 else
 {
-while (tempNode != NULL)
+free(tmp->value);
+tmp->value = strdup(element->value);
+free(element->value);
+free(element->key);
+free(element);
+}
+}
+else
 {
-if (strcmp(key, tempNode->key) == 0)
-{
-free(currNode->key);
-free(tempNode->value);
-tempNode->value = currNode->value;
-free(currNode);
-return (1);
+element->next = NULL;
+ht->array[index] = element;
 }
-tempNode = tempNode->next;
-}
-currNode->next = ht->array[index];
-ht->array[index] = currNode;
-}
-return (1);
 }
