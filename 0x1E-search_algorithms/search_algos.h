2928652651d9c4891511cafdef5001c09390f2d7
FQX117 <blackfox950@gmail.com>
2022-10-27 11:42:08 -0500

    test

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,6 +5,23 @@
 #include <stdlib.h>
 #include <math.h>
 
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    size_t index;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
 
 /** prototypes **/
 int linear_search(int *array, size_t size, int value);
@@ -12,5 +29,6 @@ int binary_search(int *array, size_t size, int value);
 int binary_search_recursion(int *array, size_t min, size_t max, int value);
 int advanced_binary(int *array, size_t size, int value);
 int binary_search_recursion(int *array, size_t min, size_t max, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
 #endif
